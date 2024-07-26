0d30788fa2cdecb2cd0d2b39fda58dca41d6c8b1
Aditya Shekhar <132371904+itsofficialadi@users.noreply.github.com>
2024-07-26 14:54:44 +0530

    resolved the CustomCursor issue over the iframe.

--- src/components/CarbonFootprintCalculator.jsx
+++ src/components/CarbonFootprintCalculator.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const CarbonFootprintCalculator = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white bg-opacity-75 p-8 rounded-lg shadow-lg w-full max-w-4xl">
-        <iframe
-          style={{ height: '1000px', width: '100%' }}
-          src="https://sustainabletravel.org/wp-content/plugins/sti_client/src/shortcode/shortcode.php?api_key=STIKEY_66a3599d5d035282884803&primary_color=%23001685&secondary_color=%2382f722&light_primary_color=%23cce2ff&sort_order=Flight%2CCar%2CBoat"
-          title="Carbon Footprint Calculator"
-        ></iframe>
-      </div>
+    <div className="relative"> {/* Make this div a positioned parent */}
+      <iframe
+        title="Carbon Footprint Calculator"
+        style={{ height: '730px', width: '100%', pointerEvents: 'none' }}
+        src="https://sustainabletravel.org/wp-content/plugins/sti_client/src/shortcode/shortcode.php?api_key=STIKEY_66a3599d5d035282884803&primary_color=%23001685&secondary_color=%2382f722&light_primary_color=%23cce2ff&sort_order=Flight%2CCar%2CBoat"
+      />
+      <div className="iframe-overlay"></div>
     </div>
   );
 };
